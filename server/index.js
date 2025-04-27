1bc69f93daeaa8006ffa0db1a0fde42deaf91bd8
Samad Khan <samadk619@gmail.com>
2025-04-27 15:46:28 +0530

    chat api, query retrieval

--- server/index.js
+++ server/index.js
@@ -2,8 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
 import { Queue } from 'bullmq';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { QdrantVectorStore } from '@langchain/qdrant';
+import OpenAI from 'openai';
 
-
+const client = new OpenAI({
+  apiKey: '',
+});
 const queue = new Queue('file-upload-queue', {
   connection: {
     host: 'localhost',
@@ -12,22 +17,23 @@ const queue = new Queue('file-upload-queue', {
 });
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-      cb(null, `${uniqueSuffix}-${file.originalname}`);
-    },
-  });
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
+  },
+});
+
 const upload = multer({ storage: storage });
+
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => { 
+app.get('/', (req, res) => {
   return res.json({ status: 'All Good!' });
 });
-
 
 app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
   await queue.add(
@@ -39,6 +45,45 @@ app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
     })
   );
   return res.json({ message: 'uploaded' });
+});
+
+app.get('/chat', async (req, res) => {
+  const userQuery = req.query.message;
+
+  const embeddings = new OpenAIEmbeddings({
+    model: 'text-embedding-3-small',
+    apiKey: '',
+  });
+  const vectorStore = await QdrantVectorStore.fromExistingCollection(
+    embeddings,
+    {
+      url: 'http://localhost:6333',
+      collectionName: 'langchainjs-testing',
+    }
+  );
+  const ret = vectorStore.asRetriever({
+    k: 2,
+  });
+  const result = await ret.invoke(userQuery);
+
+  const SYSTEM_PROMPT = `
+  You are helfull AI Assistant who answeres the user query based on the available context from PDF File.
+  Context:
+  ${JSON.stringify(result)}
+  `;
+
+  const chatResult = await client.chat.completions.create({
+    model: 'gpt-4.1',
+    messages: [
+      { role: 'system', content: SYSTEM_PROMPT },
+      { role: 'user', content: userQuery },
+    ],
+  });
+
+  return res.json({
+    message: chatResult.choices[0].message.content,
+    docs: result,
+  });
 });
 
 app.listen(8000, () => console.log(`Server started on PORT:${8000}`));
